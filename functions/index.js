b374347c103f63d9d570e2d762f65b0ec9302857
Raymund Micoh Alvarez <micohalvarez@yahoo.com>
2019-04-08 23:52:18 +0800

    test

--- functions/index.js
+++ functions/index.js
@@ -50,11 +50,7 @@ exports.daily_job = functions.pubsub.topic('daily-tick').onPublish(message => {
 
         if (certificateExpiryDate >= todaysDate) {
           console.log('HELLO WORLD I PUSHED THIS DATA');
-          newNotifsRef.set({
-            driverId: driversData.key,
-            expiryDate: driversData.child('certificateExpiry').val(),
-            expiryCard: 'Certification'
-          });
+
           newNotifsRef = notifsRef.push();
         }
       });
